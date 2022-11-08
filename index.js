b3e7886489590f4f2ca15997331a661426df9bae
shahadat52 <shahadathossain.sh255@gmail.com>
2022-11-08 12:20:59 +0600

    servives api added

--- index.js
+++ index.js
@@ -1,34 +1,47 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
 const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
+
 // meddlewere
 app.use(cors());
 app.use(express.json());
 
-// user: tutorPro
-// password: V8ZPQElPtxbLUgEB
-
-const uri =
-  "mongodb+srv://:<password>@cluster0.xo63l6y.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xo63l6y.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run() {
+  try {
+    const serviceCollection = client.db("tutorPro").collection("services");
+
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+    app.get("/servicesLimit", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.limit(3).toArray();
+      res.send(services);
+    });
+  } finally {
+  }
+}
+run().catch((error) => console.error(error));
 
 app.get("/", (req, res) => {
-  res.send("Travelux server is running");
+  res.send("Tutor Pro server is running");
 });
 
 app.listen(port, () => {
-  console.log("Travlux is running at", port);
+  console.log("Tutor Pro is running at", port);
 });
